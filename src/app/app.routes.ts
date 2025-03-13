dd0b7a6a7a8c846555c7ad57c222a93775353094
vgoosakova <vgoosakova@gmail.com>
2025-03-13 15:05:42 +0200

    Create base project structure, styles

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,29 @@
-import { Routes } from '@angular/router';
+import {Routes} from '@angular/router';
+import {ContactComponent} from './pages/contact/contact.component';
+import {ProjectsComponent} from './pages/projects/projects.component';
+import {HomeComponent} from './pages/home/home.component';
+import {AboutMeComponent} from './pages/about-me/about-me.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'home',
+  },
+  {
+    path: 'home',
+    component: HomeComponent,
+  },
+  {
+    path: 'about-me',
+    component: AboutMeComponent,
+  },
+  {
+    path: 'projects',
+    component: ProjectsComponent,
+  },
+  {
+    path: 'contact-me',
+    component: ContactComponent,
+  },
+];
